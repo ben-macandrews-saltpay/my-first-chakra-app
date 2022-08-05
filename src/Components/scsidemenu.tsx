1c61aa22daa29838f8bf175b77a8f1d1ccf8f6df
Ben MacAndrews <ben.macandrews@saltpay.co>
2022-08-05 20:02:24 +0100

    feat: wraps SideMenu in ResizePanel component

--- src/Components/scsidemenu.tsx
+++ src/Components/scsidemenu.tsx
@@ -20,52 +20,51 @@ import sideMenuItems from "../../src/pages/mock-data"
 const SideMenu = () => {
     const menuChildren = sideMenuItems.addAnAPI.children;
     return (
-        
-            <VStack
-                alignItems="flex-start"
-                bg='brand.pure.white'
-                height={"full"}
-                position={"fixed"}
-                borderRight={"1px solid"}
-                borderColor={"brand.neutral.400"}
-            >
-                <Accordion
-                    allowToggle
-                    paddingRight={"16px"}
-                    paddingLeft={"44px"}
-                    width={"325px"}
-                    position={"relative"}
-                    top={"100px"}
-                >
-                    
-                    <AccordionItem borderColor={"brand.pure.white"}>
-                        <AccordionButton
-                            justifyContent="center"
-                            alignItems="center"
-                            borderRadius="32px"
-                            minWidth="100px"
-                            boxShadow="none"
-                            _expanded={{ bg: "brand.primary.background", color: "brand.primary.500" }}
-                            _active={{ bg: "brand.primary.background", color: "brand.primary.500" }}
-                            _hover={{ bg: "brand.neutral.100", color: "brand.primary.700" }}
-                            bg="brand.pure.white"
-                            color="brand.neutral.700"
+        <VStack
+            alignItems="flex-start"
+            bg='brand.pure.white'
+            height={"full"}
+            borderRight={"1px solid"}
+            borderColor={"brand.neutral.400"}>
+            <Accordion
+                allowToggle
+                paddingRight={"16px"}
+                paddingLeft={"44px"}
+                width={"full"}
+                position={"relative"}
+                top={"100px"}
+                borderColor={"brand.pure.white"}>
+                <AccordionItem
+                    overflow={"hidden"}>
+                    <AccordionButton
+                        justifyContent="left"
+                        alignItems="center"
+                        borderRadius="32px"
+                        minWidth="240px"
+                        boxShadow="none"
+                        _expanded={{ bg: "brand.primary.background", color: "brand.primary.500" }}
+                        _active={{ bg: "brand.primary.background", color: "brand.primary.500" }}
+                        _hover={{ bg: "brand.neutral.100", color: "brand.primary.700" }}
+                        bg="brand.pure.white"
+                        color="brand.neutral.700"
+                        as={"span"}>
+                        <ApplicationsIcon />
+                        <Text
                             as={"span"}
+                            textStyle="body-semibold"
+                            _active={{ bg: "brand.primary.background", color: "brand.primary.700" }}
+                            _selected={{ bg: "brand.primary.background", color: "brand.primary.700" }}
+                            marginLeft={"20px"}
                         >
-                            <ApplicationsIcon />
-                            <Text
-                                as={"span"}
-                                textStyle="body-semibold"
-                                _active={{ bg: "brand.primary.background", color: "brand.primary.700" }}
-                                _selected={{ bg: "brand.primary.background", color: "brand.primary.700" }}
-                                marginLeft={"20px"}
-                            >
-                                <Link href={`/dashboard/applications`}>Applications</Link>
-                                <AccordionIcon width={"24px"} height={"24px"} marginLeft={"20px"} />
-                            </Text>
-                        </AccordionButton>
-                        <VStack maxHeight={"200px"} overflow={"auto"} alignItems="left">
-                            <AccordionPanel data-testid={"accordion-stack-applications"}>
+                            <Link href={`/dashboard/applications`}>Applications</Link>
+                            <AccordionIcon width={"24px"} height={"24px"} marginLeft={"20px"} />
+                        </Text>
+                    </AccordionButton>
+                    <VStack maxHeight={"200px"} overflow={"auto"} alignItems="left">
+                        <AccordionPanel
+                            data-testid={"accordion-stack-applications"}
+                            bg={"brand.pure.white"}
+                        >
                             {menuChildren?.map(menuChildren => (
                                 <AccordionItem
                                     padding={"12px 16px"}
@@ -73,6 +72,8 @@ const SideMenu = () => {
                                     position={"relative"}
                                     left={"50px"}
                                     key={menuChildren}
+                                    minW="324px"
+                                    overflow="hidden"
                                 >
                                     <Link href={"#"}>
                                         <Text
@@ -88,13 +89,13 @@ const SideMenu = () => {
                                     </Link>
                                 </AccordionItem>
                             ))}
-                            </AccordionPanel>
-                        </VStack>
-                    </AccordionItem>
+                        </AccordionPanel>
+                    </VStack>
+                </AccordionItem>
 
-                </Accordion>
-            </VStack >
-        
+            </Accordion>
+        </VStack >
+
     );
 };
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import {
   ChakraProvider,
   Box,
   Stack,
+  HStack,
   VStack,
   SimpleGrid
 } from "@chakra-ui/react"
@@ -12,24 +13,35 @@ import theme from "../theme/theme"
 import ResizePanel from "../components/resizepanel"
 
 export const App = () => (
-  
+
 
   <ChakraProvider theme={theme}>
+
+    {/* NAV BAR */}
     <VStack alignItems="left" bg='brand.pure.white' spacing="0px">
       <Box p="12px" bg="brand.pure.white" boxShadow="md" zIndex="overlay" position={"fixed"} minW="100vw">
         <Navbar />
       </Box>
-      <Stack minH="100vh" alignItems='top' spacing="0px" direction="row">
+
+      {/* PAGE WRAPPER */}
+      <Stack minH="100vh" alignItems='top' spacing="0px" direction="row" bg={"brand.neutral.50"}>
 
         {/* SIDE MENU */}
-        {/* <Sidemenu /> */}
         <ResizePanel direction="e">
-        <ScSidemenu />
-</ResizePanel>
-        <ScSidemenu />
+          <Box
+            minH={"100vh"}
+            w='full'
+            pt='72px'
+            className="panel sidebar">
+            <ScSidemenu />
+          </Box>
+        </ResizePanel>
+
+
 
         {/* CONTENT */}
-        <Box minH='full' w='full' py='72px' px={['16px', '16px', '16px', '60px']} bg="white">
+
+        <Box minH='full' w='full' pt='72px' px={['16px', '16px', '16px', '60px']} bg="white">
           <SimpleGrid columns={{ sm: 4, md: 6, lg: 12 }} spacing={{ md: 4, lg: 6 }} minH="100vh" px={15} py={6}>
             <Box h='full' bg='brand.neutral.100'></Box>
             <Box h='full' bg='brand.neutral.100'></Box>
@@ -45,8 +57,21 @@ export const App = () => (
             <Box h='full' bg='brand.neutral.100'></Box>
           </SimpleGrid>
         </Box>
+
       </Stack>
+
+
+      <HStack>
+
+        <Box minW="100px"></Box>
+      </HStack>
+
+
     </VStack>
+
+
+
+
   </ChakraProvider >
 )
 
